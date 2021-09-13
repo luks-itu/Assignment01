a2e6af2edb64338a100ff06f0b5e2b0b32a80ece
luks <luks@itu.dk>
2021-09-13 16:21:27 +0200

    implemented iterators part 1 of 2, no tests

--- App/Program.cs
+++ App/Program.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace App
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            Console.WriteLine("Kører");
+            var stream1 = new List<int>() {1, 2, 3};
+            var stream2 = new List<int>() {5, 6};
+            var input = new List<List<int>>() {stream1, stream2};
+            var output = Flatten(input);
+            foreach(var entry in output)
+            {
+                Console.Write(entry + " ");
+            }
+            
         }
 
         public static bool Even(int i)
         {
             return i % 2 == 0;
+        }
+
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+        {
+            
+            foreach ( IEnumerable<T> item in items)
+            {
+                foreach (var entry in item)
+                {
+                    yield return entry;
+                }
+            }
         }
     }
 }
