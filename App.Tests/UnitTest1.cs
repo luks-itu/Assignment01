e7cf7d61abcfeb93dc26375a79f51e95858a683b
Markus <mkjo@itu.dk>
2021-09-13 17:01:31 +0200

    add Flatten() unit test

--- App.Tests/UnitTest1.cs
+++ App.Tests/UnitTest1.cs
@@ -1,10 +1,28 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace App.Tests
 {
     public class UnitTest1
     {
+        [Fact]
+        public void Flatten_given_1_2_5_and_8_9_returns_1_2_5_8_9()
+        {
+            //Arrange
+            var stream1 = new List<int>(){1,2,5};
+            var stream2 = new List<int>(){8,9};
+            var input = new List<List<int>>(){stream1, stream2};
+
+
+            //Act
+            var output = Program.Flatten(input);
+            
+            //Assert
+            Assert.Equal(output, new List<int>(){1,2,5,8,9});
+    
+        }
+
         [Fact]
         public void EvenFunction_ReturnsTrueIfEven()
         {
@@ -18,11 +36,8 @@ namespace App.Tests
 
             Assert.True(actualEven);
 
-
-
-
-
         }
+
         [Fact]
         public void EvenFunction_ReturnsFalseIfUneven()
         {
